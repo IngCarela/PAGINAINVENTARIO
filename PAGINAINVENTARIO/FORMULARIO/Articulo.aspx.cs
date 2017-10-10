978046985ff6136c762361bccc275a35fb25ceb4
IngCarela <ingcarela89@gmail.com>
2017-10-09 20:05:02 -0400

    Se pueden insertar articulos
    Esta hecho el formulario de articulos

---
+++ PAGINAINVENTARIO/FORMULARIO/Articulo.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PAGINAINVENTARIO.MODELO;
+
+namespace PAGINAINVENTARIO.FORMULARIO
+{
+    
+    public partial class Articulo : System.Web.UI.Page
+    {
+        INVENTARIODBEntities db = new INVENTARIODBEntities();
+        int precio;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            añadirArticulo();
+        }
+
+        private void añadirArticulo()
+        {
+            convertirAEntero();
+            try
+            {
+                ARTICULOS articulo = new ARTICULOS
+                {
+                    Articulo = txtArticulo.Text,
+                    Cantidad = txtCantidad.Text,
+                    Precio = precio
+                };
+
+                db.ARTICULOS.Add(articulo);
+                db.SaveChanges();
+
+                Response.Write("<script>alert('Articulo insertado')</script>");
+            }
+            catch (Exception)
+            {
+
+                Response.Write("<script>alert('Ha ocurrido un error')</script>");
+            }
+
+            limpiarCajaDeTexto();
+            focoArticulo();
+            
+        }
+
+        private void convertirAEntero()
+        {
+            precio = Convert.ToInt32(txtPrecio.Text);
+        }
+
+        private void limpiarCajaDeTexto()
+        {
+            txtArticulo.Text = txtCantidad.Text = txtPrecio.Text = "";
+        }
+
+        private void focoArticulo()
+        {
+            txtArticulo.Focus();
+        }
+
+        private void binding()
+        {
+            gvArticulo.DataSource = db.ARTICULOS.ToList();
+            gvArticulo.DataBind();
+        }
+    }
+}
---
+++ PAGINAINVENTARIO/FORMULARIO/Articulo.aspx.designer.cs
@@ -0,0 +1,96 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace PAGINAINVENTARIO.FORMULARIO {
+    
+    
+    public partial class Articulo {
+        
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// Control txtArticulo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtArticulo;
+        
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// Control txtCantidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCantidad;
+        
+        /// <summary>
+        /// Control Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// Control txtPrecio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPrecio;
+        
+        /// <summary>
+        /// Control btnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardar;
+        
+        /// <summary>
+        /// Control gvArticulo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvArticulo;
+        
+        /// <summary>
+        /// Control ArticuloSource.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource ArticuloSource;
+    }
+}
